94a7f311e58ffa366371dcb46edec47df10763cf
mariah johnson <mariahdawn9@gmail.com>
2022-11-10 01:44:53 +0000

    working all over. Added html and css +  temp images for cards

--- scripts.js
+++ scripts.js
@@ -1,2 +1,9 @@
 /*Javascript for memeory card game*/
 
+window.onload = function(){
+  
+  cardList = document.getElementsByClassName("card");
+  
+  cardCount = cardList.length; //finds how many cards are on the table
+  
+}
