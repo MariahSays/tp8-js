563841b29ee8a8afda6cea09dc045d1ee626c10b
mariah johnson <mariahdawn9@gmail.com>
2022-11-13 23:12:23 +0000

    began match check function for cards clicked(addedd from code pen)

--- scripts.js
+++ scripts.js
@@ -6,11 +6,48 @@ window.onload = function(){
   
   cardCount = cardList.length; //finds how many cards are on the table
   
-  for (c = 0; c < cardCount; c++) {
-    
-    cardList[c].onclick = function(){
-      this.classList.toggle("clicked");
+  cardsClicked = 0; // card tracker
+  
+  function cardClicked(what) {
+    //toggles front and back of card 
+    if(what.classList.contains("clicked")){
+        what.classList.remove("clicked");
+        cardsClicked--;
+       }
+    else{
+      what.classList.add("clicked");
+      cardsClicked++;
+       if (cardsClicked === 2){ 
+          cardCheck();
+       }
     }
   }
- 
+  
+ function cardCheck(cardsClicked){
+   //function to check if cards match
+   clickedCards = document.getElementsByClassName("clicked");
+   
+   matched = false;
+   if (clickedCards[0].classList == clickedCards[1].classList){
+     //compares classes of first and second clicked cards
+     matched = true;
+   }
+   else {
+     matched =false;
+   }
+   if (matched){
+     
+     removeMatch();
+   }
+   else{
+     flipCards()
+   }
+}
+  
+  for (c = 0; c < cardCount; c++) {
+    //flips cards for number of cards 
+    cardList[c].onclick = function(){
+      cardClicked(this);
+    }  
+  }
 }
